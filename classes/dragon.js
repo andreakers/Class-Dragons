9c89ebe19ff99c6a6ee633f42dd4650ae18ac665
Brandon Espinosa <espinbrandon49@gmail.com>
2024-04-27 12:39:26 -0400

    create dragon class

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,26 @@
-// Your code here 
+class Dragon {
+
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let newArray = []
+
+    for (let i = 0; i < dragons.length; i++) {
+      let dragon = dragons[i]
+      newArray.push(dragon.name)
+    }
+    return newArray
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
